2fd1c7d4e9c2040c61a05441e050aa3144c0fae5
Sri Prabhu Muthukumar <sriprabhumuthukumar@gmail.com>
2026-06-11 03:24:35 +0530

    jenkins

---
+++ data/payment.js
@@ -0,0 +1,13 @@
+const paymentData ={
+
+    cardName :"Sri prabhu",
+    cardNum : "9654789456123",
+    cardCvc : "458",
+    cardMonth :"05",
+    cardYear : "2055"
+
+     //sri prabhu
+
+}
+
+export default paymentData
